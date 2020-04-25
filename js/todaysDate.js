18b6be46060c0a1c1e8dbb401d1a1b511a69106f
Deaton879 <Deaton879@byui.edu>
2020-04-24 22:29:13 -0400

    update

--- js/todaysDate.js
+++ js/todaysDate.js
@@ -1,2 +1,2 @@
 const options ={weekday: 'short', day: 'numeric', month: 'long', year: 'numeric'};
-document.getElementById('todaysDate').textContent = new Date().toLocaleDateString('en-US', options); 
+document.getElementById('todaysDate').textContent = new Date().toLocaleDateString('en-US', options) + " " + "|" +  " " + new Date().toLocaleTimeString(); 
