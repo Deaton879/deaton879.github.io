5ee90aa67caa5eeab54acb2ea249b062387377ff
Deaton879 <Deaton879@byui.edu>
2020-05-14 18:42:32 -0400

    Updated for Preston

---
+++ Lesson4/js/currentDate.js
@@ -0,0 +1,4 @@
+const options ={weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
+
+document.getElementById('todaysDate').textContent = 
+new Date().toLocaleDateString('en-US', options)
