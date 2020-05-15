9935d9b7c36eccbc9412325feafb7e43d9271fd9
Deaton879 <Deaton879@byui.edu>
2020-05-14 21:13:39 -0400

    All done

--- Lesson4/js/currentDate.js
+++ Lesson4/js/currentDate.js
@@ -1,4 +1,12 @@
-const options ={weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
+document.getElementById("todaysDate").textContent = todayDate();
 
-document.getElementById('todaysDate').textContent = 
-new Date().toLocaleDateString('en-US', options)
+function todayDate() {
+    var today = new Date();
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const months = ["January", "February", "March", "April", "May", "June","July", "August", "September", "October", "November", "December"];
+    var month = months[today.getMonth()];
+    var day = days[today.getDay()];
+    var dayDate = today.getDate();
+    var year = today.getFullYear();
+    return  day + ", " + dayDate + " " + month + " " + year;
+}
