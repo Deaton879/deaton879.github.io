d3d6d89f0d3f9265f283953a8b868ec705566bf3
Deaton879 <Deaton879@byui.edu>
2020-06-04 10:29:57 -0400

    lazy load update

--- Lesson7/js/lazyload.js
+++ Lesson7/js/lazyload.js
@@ -12,8 +12,7 @@ function preloadImage(img) {
 }
 
 const imgOptions = {
-    threshold: 0,
-    rootMargin: "0px 0px -200px 0px"
+    threshold: 0.50
 };
 
 const imgObserver = new IntersectionObserver((entries, 
