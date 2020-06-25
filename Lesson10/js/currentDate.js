a1a26b816e0e70d99f45828f5a2e3ca303a86008
Deaton879 <Deaton879@byui.edu>
2020-06-25 16:12:25 -0400

    finished week 10

--- Lesson10/js/currentDate.js
+++ Lesson10/js/currentDate.js
@@ -8,5 +8,39 @@ function todayDate() {
     var day = days[today.getDay()];
     var dayDate = today.getDate();
     var year = today.getFullYear();
-    return  day + ", " + dayDate + " " + month + " " + year;
+
+    //For setting correct day on weather forecast
+    const fday = ['Sun', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat'];
+    var nday =  today.getDay();
+
+    document.getElementById('day1').textContent = fday[nday]; //first day of 5-day forecast (current date)
+
+    let a = document.getElementById('day2');
+    let b = document.getElementById('day3');
+    let c = document.getElementById('day4');
+    let d = document.getElementById('day5');
+
+    const forecast = [a, b, c, d]; //list of remainding forecast spots
+
+    let x = (nday + 1);
+    let looped = 0;
+
+    /* For each forecast date, add 1 to x then increment. if x exceeds 6 (saturday), else if picks back up at week start on 0 (sunday) with looped, which is incremented until the for statement expires */
+    for(let i = 0; i < forecast.length; i++) {
+        if(x <= 6) {
+            console.log(fday[x])
+            forecast[i].textContent = fday[x];
+            x++;
+        }
+        else if (x > 6) {
+            console.log(fday[looped])
+            forecast[i].textContent = fday[looped];
+            looped++;
+        }
+    }
+    return  day + ", " + dayDate + " " + month + " " + year;       
+}
+
+function forecaster(num) {
+
 }
--- Lesson10/js/weather_api.js
+++ Lesson10/js/weather_api.js
@@ -1,8 +1,8 @@
 // FOR THE CURRENT WEATHER IN HERO IMAGE
 
-function getCurrent() {
+function getCurrent(x) {
   //USE WEATHER IN URL QUERY
-  const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=5a73ef4b83ca4f2d6941744d6dc8e118";
+  const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=" + x + "&units=imperial&appid=5a73ef4b83ca4f2d6941744d6dc8e118";
   
   /*TEST FILE
   const apiURL = "./json/preston1.json";*/
@@ -15,10 +15,10 @@ function getCurrent() {
       console.log(jsObject);
 
       document.getElementById('currently').textContent = jsObject.weather[0].description;
-      document.getElementById('temp-now').textContent = jsObject.main.temp + '℉';
-      document.getElementById('temp-high').textContent = jsObject.main.temp_max + '℉';
+      document.getElementById('temp-now').textContent = Math.round(jsObject.main.temp) + '℉';
+      document.getElementById('temp-high').textContent = Math.round(jsObject.main.temp_max) + '℉';
       document.getElementById('humidity').textContent = jsObject.main.humidity + '%';
-      document.getElementById('windspeed').textContent = jsObject.wind.speed + "mph";
+      document.getElementById('windspeed').textContent = Math.round(jsObject.wind.speed) + "mph";
 
 
       //CODE FOR WINDCHILL CALCULATION
@@ -46,9 +46,9 @@ function getCurrent() {
 
 // FOR THE 5-DAY FORECAST
 
-function getFive() {
+function getFive(x) {
   //USE FORECAST IN URL QUERY
-  const apiURL = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=5a73ef4b83ca4f2d6941744d6dc8e118";
+  const apiURL = "https://api.openweathermap.org/data/2.5/forecast?id=" + x + "&units=imperial&appid=5a73ef4b83ca4f2d6941744d6dc8e118";
 
   /*TEST FILE
   const apiURL = "./json/preston2.json";*/
@@ -65,8 +65,8 @@ function getFive() {
 
       //ARRAY FOR childNodes from div#fiveDay
       let fiveday = document.getElementById('fiveDay').childNodes;
-      /*log for reference
-      console.log(fiveday);*/
+      //log for reference
+      console.log(fiveday);
 
       let forecast = jsObject.list.filter((time) => {
         return (time.dt_txt.includes(x));
@@ -78,14 +78,14 @@ function getFive() {
       /*LET 'i' TO LOOP THROUGH FIVEDAY (ADD 2, i+=2 to bypass the #TEXT childNodes that start and surround each defined node, otherwise undefined nodes would be passed and throw an error)*/
       for(let i = 1; i<=9; i+=2) {
         //TEMPERATURE
-        fiveday[i].childNodes[5].textContent = forecast[n].main.temp + "℉";
+        fiveday[i].childNodes[5].textContent = Math.round(forecast[n].main.temp) + "℉";
         //CONSTRUCT THE THUMBNAIL URL ADDRESS
         let imgsrc = 'https://openweathermap.org/img/wn/' + forecast[n].weather[0].icon + '@2x.png';
         let desc = forecast[n].weather[0].description;
         fiveday[i].childNodes[3].childNodes[1].setAttribute('src', imgsrc);
         fiveday[i].childNodes[3].childNodes[1].setAttribute('alt', desc);
         //INCREMENT FORECAST IDENTIFER
-        n += 1;
+        n++;
       }
 
   });
